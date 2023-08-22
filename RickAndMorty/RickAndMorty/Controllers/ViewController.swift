457f1db44d28fd6cee5440df12dde6610be5fd07
Paulo Henrique Bendazzoli <Paulo.Bendazzoli-ext@ab-inbev.com>
2023-08-22 19:08:04 -0300

    Initial project folders struct

---
+++ RickAndMorty/RickAndMorty/Controllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Paulo Henrique Bendazzoli on 22/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
